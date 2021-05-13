1084f39e87f40c4c656aea5d7c48b4a15cd7354f
Pedroosorio97 <pedromrnosorio97@gmail.com>
2021-05-13 15:42:45 +0100

    First Commit

---
+++ GameofUr/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameofUr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
