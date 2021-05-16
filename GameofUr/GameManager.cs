6cf5ec8b51146b2602a611113fa3fca39669d95c
ritikjoshi24 <74405351+ritikjoshi24@users.noreply.github.com>
2021-05-16 22:30:29 +0100

    Added GameManager correctly

---
+++ GameofUr/GameManager.cs
@@ -0,0 +1,57 @@
+<<<<<<< HEAD
+using System;
+using System.Collections.Generic;
+
+namespace GameofUr
+{
+    public class GameManager
+    {
+        public string input;
+        string input1;
+        View view = new View();
+        Board board = new Board();
+
+
+        public GameManager()
+        {
+        }
+        public void game() // here is another menu which is the play menu
+        {
+
+            view.sos();
+            do
+            {
+                input1 = view.takinginput(input);
+
+                switch (input1)
+                {
+                    case "play":
+                        board.displayBoard();
+                        view.sos();
+                        break;
+
+                    case "show":
+                        view.sos();
+                        break;
+
+                    case "help":
+                        view.help();
+                        break;
+
+                    case "quit":
+                        System.Environment.Exit(0);
+                        break;
+                }
+            } while (input1 != "quit");
+        }
+
+    }
+=======
+namespace 
+{
+    public class GameManager
+    {
+        
+    }
+>>>>>>> 942fc46fb668e6413ca0ade8c99a5903c49f85ac
+}
